f8ff86a1d09f3611f8ba1653ef1fcdd15f12a524
robe1606 <robe1606@morris.umn.edu>
2019-09-11 17:08:18 -0500

    Improve process_client_log.sh and remove extra print statements from the test file.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,11 +2,10 @@
 
 logDir=$1
 
-#start_directory=$(pwd)
-
-#cd $logDir || exit
 cd $logDir
 
-#tar xf *.tgz
 
-cat var/log/secure* | sed -E -n 's/([a-zA-Z]+)(\s)(\d+)(\s)([0-9][0-9]):[0-9][0-9]:[0-9][0-9] computer_name sshd\[[0-9]{5}\]: Failed password for (invalid user )?([a-zA-Z0-9 _-]*) from ([0-9.]*) port [0-9]* ssh2/\1 \3 \5 \7 \8/p' > failed_login_data.txt
+cat var/log/secure* | sed -E -n 's/(^[a-zA-Z]*) ([0-9]*) ([0-9]*):[0-9]*:[0-9]* computer_name sshd[^:]*: Failed password for (invalid user )?([A-Za-z_-]*) from ([0-9.]*) port [0-9]* ssh2/\1\2\3\5\6/p' > failed_login_data.txt
+
+
+
