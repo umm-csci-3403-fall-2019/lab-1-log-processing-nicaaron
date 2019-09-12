e5509408cb2ab4118f71482a478b49c2168ae9c7
robe1606 <robe1606@morris.umn.edu>
2019-09-12 11:35:28 -0500

    Make create_country_dist.sh more readable

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -6,7 +6,10 @@ countryDir=$1
 
 cd $countryDir
 
-cat */failed_login_data.txt | sed -E -n 's/[a-zA-Z]* [0-9]* [0-9]* [a-zA-Z0-9_-]* ([0-9.]*)/\1/p' | sort | uniq -c | sed -E -n "s/[ ]*([0-9]*) ([0-9]*)/data.addRow(['\2', \1]);/p" > country_dist.txt
+cat */failed_login_data.txt | sed -E -n 's/[a-zA-Z]* [0-9]* [0-9]* [a-zA-Z0-9_-]* ([0-9.]*)/\1/p' \
+| sort \
+| uniq -c \
+| sed -E -n "s/[ ]*([0-9]*) ([0-9]*)/data.addRow(['\2', \1]);/p" > country_dist.txt
 
 cd $work_dir
 
