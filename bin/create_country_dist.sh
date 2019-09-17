5b842c81a1a200763eb4b0b1b329a8116593415e
robe1606 <robe1606@morris.umn.edu>
2019-09-17 11:27:46 -0500

    Add join function to create_country_dist.sh. Update regex functions and data.addrow()

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -6,10 +6,11 @@ countryDir=$1
 
 cd $countryDir
 
-cat */failed_login_data.txt | sed -E -n 's/[a-zA-Z]* [0-9]* [0-9]* [a-zA-Z0-9_-]* ([0-9.]*)/\1/p' \
-| sort \
+cat */failed_login_data.txt | sed -E -n 's/[a-zA-Z]* [0-9]* [0-9]* [a-zA-Z0-9_-]* ([0-9.]*)/\1/p' > failed_ip.txt\
+| sort failed_ip.txt\
 | uniq -c \
-| sed -E -n "s/[ ]*([0-9]*) ([0-9]*)/data.addRow(['\2', \1]);/p" > country_dist.txt
+| join failed_ip.txt etc/country_IP_map.txt 
+| sed -E -n "s/([0-9]*) ([0-9.]*) ([A-Z]*)/data.addRow(['\3', \1]);/p" > country_dist.txt
 
 cd $work_dir
 
