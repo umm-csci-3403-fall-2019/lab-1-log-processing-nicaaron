482247e7ac96da15f04a1ec024217ce5918deb02
robe1606 <robe1606@morris.umn.edu>
2019-09-17 17:35:18 -0500

    Fix multiple syntax errors in create_country_dist.sh

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
+#Working directory
 work_dir=$(pwd)
 
+#First input directory
 countryDir=$1
 
-cd $countryDir
+#Enter input directory
+cd "$countryDir" || exit
 
-cat */failed_login_data.txt | sed -E -n 's/[a-zA-Z]* [0-9]* [0-9]* [a-zA-Z0-9_-]* ([0-9.]*)/\1/p' > failed_ip.txt\
-| sort failed_ip.txt\
-| uniq -c \
-| join failed_ip.txt etc/country_IP_map.txt 
-| sed -E -n "s/([0-9]*) ([0-9.]*) ([A-Z]*)/data.addRow(['\3', \1]);/p" > country_dist.txt
+#Match failed login data and sort it (took the IP address). Join piped data with country IP map. Pull country data from joined file, sorts it and finds unique entries. Adds data to country_dist.txt in a spreadsheet.
+cat ./*/failed_login_data.txt \
+	| sed -E -n 's/[A-Za-z]*[ ]*[0-9]* [0-9]* [A-Za-z0-9_-]* ([0-9.]*)/\1/p' \
+        | sort \
+	| join - "$work_dir"/etc/country_IP_map.txt \
+	| sed -E -n 's/[0-9.]* ([A-Z]*)/\1/p' \
+	| sort \
+        | uniq -c \
+        | sed -E -n "s/[ ]*([0-9]*) ([A-Z]*)/data.addRow(['\2', \1]);/p" \
+        > country_dist.txt
 
-cd $work_dir
+#Enter working directory and wrap contents.
+cd "$work_dir" || exit
 
-./bin/wrap_contents.sh $countryDir/country_dist.txt html_components/country_dist $countryDir/country_dist.html
+./bin/wrap_contents.sh "$countryDir"/country_dist.txt html_components/country_dist "$countryDir"/country_dist.html
+
+#Remove text file.
+rm "$countryDir"/country_dist.txt
